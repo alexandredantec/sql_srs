a415904374ad1ee50f5fbbedcd58e71df96a4c61
alexandre.dantec <alexandre.dantec@hokodo.co>
2024-06-13 10:03:56 +0200

    add sql functionality

--- sql_srs/app.py
+++ sql_srs/app.py
@@ -1,13 +1,19 @@
 import streamlit as st
+
 import pandas as pd
+import duckdb
 
 st.write("Hello world!")
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
 
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
-    input_text = st.text_area(label="enter your input")
-    st.write(input_text)
+    sql_query = st.text_area(label="enter your input")
+    result = duckdb.query(sql_query).df()
+    st.write(f"You have entered the following SQL query: {sql_query}")
+    st.dataframe(result)
 
 with tab2:
     st.header("A dog")
